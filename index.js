43a029d0d1ae6b31e66bba2c9757d480da49067d
14justin <justinmwangi40@gmail.com>
2024-03-21 13:12:57 +0300

    test

---
+++ index.js
@@ -0,0 +1,24 @@
+
+    
+     let divide  =()  => 2000/100
+     let divides = (a,b) =>a/b;
+
+    let square =a => a*a;
+
+  
+
+    let  add = (a,b) =>(a+b);
+      add => (a+b)
+     
+
+
+
+
+
+
+
+
+
+
+     
+ 
